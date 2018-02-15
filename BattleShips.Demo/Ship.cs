65ee56ae356b2108a493cb5ceeead14be35848cb
ObsidianPhoenix <5765633+ObsidianPhoenix@users.noreply.github.com>
2018-02-15 20:48:51 +0000

    Initial Commit.

---
+++ BattleShips.Demo/Ship.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BattleShips.Demo
+{
+    public class Ship
+    {
+        public event EventHandler ShipSunk;
+
+        public string Name => Type.ToString();
+
+        public int Length => (int)Type;
+
+        public ShipType Type { get; }
+
+        private List<GridPoint> Points { get; set; }
+
+        public Ship(ShipType type)
+        {
+            Type = type;
+        }
+        
+        public void SetPoints(IEnumerable<GridPoint> points)
+        {
+            Points = points.ToList();
+            Points.ForEach(p => p.PointHit += PointHit);
+        }
+
+        private void PointHit(object sender, EventArgs e)
+        {
+            if (Points.All(p => p.HasBeenHit))
+                ShipSunk?.Invoke(this, new EventArgs());
+        }
+    }
+
+    public class GridPoint
+    {
+        public event EventHandler PointHit;
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public bool HasBeenHit { get; private set; }
+        
+        public GridPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Hit()
+        {
+            HasBeenHit = true;
+
+            PointHit?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ BattleShips.Demo/ShipType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShips.Demo
+{
+    public enum ShipType
+    {
+        Destroyer = 2,
+        Submarine = 3,
+        Cruiser = 3, //This works, but only because we need cast from name to value. If we need to go from value to name, enum could cause issues.
+        Battleship = 4,
+        Carrier = 5
+    }
+}
